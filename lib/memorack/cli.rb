a657e3a5b08e57b201527d649ebab55d677f9261
gnue <gnue@so-kukan.com>
2013-01-27 13:35:46 +0900

    実行コマンドに server サブコマンドを追加

--- lib/memorack/cli.rb
+++ lib/memorack/cli.rb
@@ -6,20 +6,24 @@ module Memorack
 
       # NOTE: the option -p/--path= is given as an example, and should be replaced in your application.
 
-      options = {port: 9292}
+      options = {port: 9292, theme: 'oreilly'}
       mandatory_options = %w(  )
 
       parser = OptionParser.new do |opts|
         opts.banner = <<-BANNER.gsub(/^          /,'')
-          This application is wonderful because...
-
-          Usage: #{File.basename($0)} [options]
-
-          Options are:
+          Usage: #{File.basename($0)} create PATH
+                 #{File.basename($0)} server [options] PATH
         BANNER
+
         opts.separator ""
+        opts.separator "Server options:"
         opts.on("-p", "--port PORT", String,
-                "Use server port") { |arg| options[:port] = arg }
+                "use PORT (default: #{options[:port]})") { |arg| options[:port] = arg }
+        opts.on("-t", "--theme THEME", String,
+                "use THEME (default: oreilly)") { |arg| options[:theme] = arg }
+
+        opts.separator ""
+        opts.separator "Common options:"
         opts.on("-h", "--help",
                 "Show this help message.") { stdout.puts opts; exit }
         opts.parse!(arguments)
@@ -34,10 +38,18 @@ module Memorack
       # do stuff
       case subcmd
       when 'create'
-		require 'fileutils'
-      	FileUtils.copy_entry(File.expand_path('../template', __FILE__), path)
-	    stdout.puts "created #{path}"
-	  else
+        require 'fileutils'
+        FileUtils.copy_entry(File.expand_path('../template', __FILE__), path)
+        stdout.puts "created #{path}"
+      when 'server'
+        require 'rack/builder'
+        require 'rack/handler/webrick'
+        app = Rack::Builder.new {
+          require 'memorack'
+          run MemoRack::MemoApp.new(nil, theme: options[:theme], root: path)
+        }
+        Rack::Server.new(:app => app, :Port => options[:port]).start
+      else
         stdout.puts parser.help
       end
     end
