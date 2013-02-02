27e5d7f5f2ef3b88c08c2803a81b88708d0bfafc
gnue <gnue@so-kukan.com>
2013-02-02 13:18:25 +0900

    newgem から bundle gem で生成した構成に変更

--- lib/memorack.rb
+++ lib/memorack.rb
@@ -1,8 +1,6 @@
-$:.unshift(File.dirname(__FILE__)) unless
-  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+require "memorack/version"
+require 'memorack/memoapp'
 
 module MemoRack
-  VERSION = '0.0.1'
+  # Your code goes here...
 end
-
-require 'memorack/memoapp'
---
+++ lib/memorack/version.rb
@@ -0,0 +1,3 @@
+module MemoRack
+  VERSION = "0.0.1"
+end
