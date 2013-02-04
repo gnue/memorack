c2eb65ccac206a3085b7cd07389b0f485f175af7
gnue <gnue@so-kukan.com>
2013-02-04 19:03:21 +0900

    CLI をリファクタリング

--- lib/memorack/cli.rb
+++ lib/memorack/cli.rb
@@ -3,16 +3,8 @@ require 'optparse'
 module MemoRack
 	class CLI
 
-		def self.execute(argv = [])
-			options = {theme: 'oreilly'}
-			mandatory_options = %w(  )
-
-			server_options = {
-				:environment	=> ENV['RACK_ENV'] || 'development',
-				:Port			=> 9292,
-				:Host			=> '0.0.0.0',
-				:AccessLog		=> [],
-			}
+		def self.run(argv = ARGV)
+			subcmd = nil
 
 			parser = OptionParser.new do |opts|
 				opts.version = MemoRack::VERSION
@@ -25,27 +17,66 @@ module MemoRack
 				opts.separator ""
 				opts.on("-h", "--help", "Show this help message.") { abort opts.help }
 
-				if mandatory_options && mandatory_options.find { |option| options[option.to_sym].nil? }
-					abort opts.help
-				end
+				opts.order!(argv)
+
+				subcmd = argv.shift
+				abort opts.help unless subcmd
+				abort opts.help unless self.has_action?(subcmd)
 			end
 
-			# コマンド名
-			cmd = parser.program_name
+			cli = self.new
+			cli.action(subcmd, *argv)
+		end
 
-			# サブコマンドのオプション解析
-			subparsers = Hash.new {|h,k| abort "#{cmd}: '#{k}' is not a #{cmd} command. See '#{cmd} --help'" }
+		# サブコマンドが定義されているか？
+		def self.has_action?(command)
+			method_defined? "memorack_#{command}"
+		end
 
-			# create のオプション解析
-			subparsers['create'] = OptionParser.new do |opts|
-				opts.banner = "Usage: #{cmd} create [options] PATH"
+		# サブコマンドの実行
+		def action(command, *args)
+			command = command.gsub(/-/, '_')
+			send "memorack_#{command}", *args
+		end
 
+		# テンプレートの作成
+		def memorack_create(*args)
+			options = {}
+			path = nil
+
+			# オプション解析
+			OptionParser.new do |opts|
+				opts.banner = "Usage: #{opts.program_name} create [options] PATH"
 				opts.on("-h", "--help", "Show this help message.") { abort opts.help }
+
+				opts.order!(args)
+
+				path = args.shift
+				abort opts.help unless path
 			end
 
-			# server のオプション解析
-			subparsers['server'] = OptionParser.new do |opts|
-				opts.banner = "Usage: #{cmd} server [options] PATH"
+			abort "File exists '#{path}'" if File.exists?(path)
+
+			require 'fileutils'
+			FileUtils.copy_entry(File.expand_path('../template', __FILE__), path)
+			puts "Created '#{path}'"
+		end
+
+		# サーバーの実行
+		def memorack_server(*args)
+			options = {theme: 'oreilly'}
+			path = nil
+
+			server_options = {
+				:environment	=> ENV['RACK_ENV'] || 'development',
+				:Port			=> 9292,
+				:Host			=> '0.0.0.0',
+				:AccessLog		=> [],
+			}
+
+			# オプション解析
+			OptionParser.new do |opts|
+				opts.banner = "Usage: #{opts.program_name} server [options] PATH"
 
 				opts.separator ""
 				opts.separator "Server options:"
@@ -54,45 +85,27 @@ module MemoRack
 				opts.on("-t", "--theme THEME", String,
 						"use THEME (default: oreilly)") { |arg| options[:theme] = arg }
 				opts.on("-h", "--help", "Show this help message.") { abort opts.help }
+
+				opts.order!(args)
+
+				path = args.shift
+				abort opts.help unless path
 			end
 
-			parser.order!(argv)
-			abort parser.help if argv.empty?
+			# サーバーの起動
+			require 'rack/builder'
+			require 'rack/handler/webrick'
+			app = Rack::Builder.new {
+				require 'memorack'
+				require 'tmpdir'
 
-			subcmd = argv.shift
-			subparser = subparsers[subcmd]
-			subparser.parse!(argv)
+				Dir.mktmpdir do |tmpdir|
+					run MemoRack::MemoApp.new(nil, theme: options[:theme], root: path, tmpdir: tmpdir)
+				end
+			}
 
-			# do stuff
-			case subcmd
-			when 'create'
-				path = argv.shift
-				abort subparser.help unless path
-			abort "File exists '#{path}'" if File.exists?(path)
-
-				require 'fileutils'
-				FileUtils.copy_entry(File.expand_path('../template', __FILE__), path)
-				puts "Created '#{path}'"
-			when 'server'
-				path = argv.shift
-				abort subparser.help unless path
-
-				require 'rack/builder'
-				require 'rack/handler/webrick'
-				app = Rack::Builder.new {
-					require 'memorack'
-					require 'tmpdir'
-
-					Dir.mktmpdir do |tmpdir|
-						run MemoRack::MemoApp.new(nil, theme: options[:theme], root: path, tmpdir: tmpdir)
-					end
-				}
-
-				server_options[:app] = app
-				Rack::Server.new(server_options).start
-			else
-				abort parser.help
-			end
+			server_options[:app] = app
+			Rack::Server.new(server_options).start
 		end
 	end
 end
