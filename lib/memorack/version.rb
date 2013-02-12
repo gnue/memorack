58db7f9f9f0c900d3a1681017646a7b6cb842050
gnue <gnue@so-kukan.com>
2013-02-12 22:21:10 +0900

    VERSION の生成方法を変更

--- lib/memorack/version.rb
+++ lib/memorack/version.rb
@@ -1,6 +1,6 @@
 module MemoRack
 	LONG_VERSION = File.read(File.expand_path('../../../VERSION', __FILE__)).chomp
-	LONG_VERSION.scan(/^([0-9.]+)/) { |match| VERSION = match[0] }
+	VERSION = LONG_VERSION.gsub(/-.*$/, '')
 
 	HOMEPAGE = 'https://github.com/gnue/memorack'
 end
