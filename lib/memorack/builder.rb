5fd8f8683d0a73d0dfbfca5f6fef8f150a85101b
gnue <gnue@so-kukan.com>
2013-04-13 14:09:06 +0900

    content ディレクトリ内のファイルをテンプレートエンジンで変換して html 出力できるようにした

--- lib/memorack/builder.rb
+++ lib/memorack/builder.rb
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 
+require 'fileutils'
 require 'pathname'
 require 'rubygems'
 
@@ -8,7 +9,36 @@ require 'memorack/core'
 module MemoRack
 	class Builder < Core
 
+		DEFAULT_BUILD_OPTIONS = {
+			output:		'site',
+			prefix:		'',
+			suffix:		'.html',
+			uri_escape:	true,
+		}
+
 		def generate(options = {})
+			options = DEFAULT_BUILD_OPTIONS.merge(options)
+
+			output = options[:output]
+			FileUtils.mkpath(output)
+
+			@contents = contents(options)
+
+			@contents.files.each { |file|
+				path = file[:path]
+				content = render_content({}, path)
+				next unless content
+
+				yield(file) if block_given?
+
+				begin
+					to = path.sub(/\.[^.]*$/, '') + options[:suffix]
+					to = File.join(output, to)
+					FileUtils.mkpath(File.dirname(to))
+					File.write(to, content)
+				rescue
+				end
+			}
 		end
 
 		#### テンプレート
@@ -24,7 +54,7 @@ module MemoRack
 
 		# メニューを作成
 		template :menu do
-			contents.generate(StringIO.new).string
+			@contents.generate(StringIO.new).string
 		end
 
 	end
