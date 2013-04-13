95d69e04618cfdbba414c3be9296a0b8e32c902e
gnue <gnue@so-kukan.com>
2013-04-13 20:12:55 +0900

    content_write の引数で suffix を指定するようにした

--- lib/memorack/builder.rb
+++ lib/memorack/builder.rb
@@ -26,14 +26,14 @@ module MemoRack
 			@contents = contents(options)
 			@templates = Set.new @contents.files.collect { |file| file[:path] }
 
-			content_write(:index, options) { |template|
+			content_write(:index, options[:suffix], options) { |template|
 				render_with_mustache template, :markdown
 			}
 
 			@templates.each { |path|
 				yield(path) if block_given?
 
-				content_write(path, options) { |template|
+				content_write(path, options[:suffix], options) { |template|
 					render_content({}, template)
 				}
 			}
@@ -42,7 +42,7 @@ module MemoRack
 		end
 
 		# コンテンツをファイルに出力する
-		def content_write(template, options)
+		def content_write(template, suffix, options)
 			begin
 				content = yield(template)
 				return unless content
@@ -50,7 +50,7 @@ module MemoRack
 				path = template
 				path = path.sub(/\.[^.]*$/, '') if path.kind_of?(String)
 
-				to = path.to_s + options[:suffix]
+				to = path.to_s + suffix
 				to = File.join(options[:output], to)
 
 				FileUtils.mkpath(File.dirname(to))
