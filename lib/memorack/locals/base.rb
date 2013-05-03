d6d1d83ae56393ad460a1ea885182e3de449e290
gnue <gnue@so-kukan.com>
2013-05-03 11:08:39 +0900

    くずぱんリスト(breadcrumb) のために nav と subdirs をテンプレートで参照できるようにした

--- lib/memorack/locals/base.rb
+++ lib/memorack/locals/base.rb
@@ -20,6 +20,40 @@ module MemoRack
 		define_key :__menu__ do |key|
 			@app.render_menu
 		end
+
+		define_key :subdirs do |key|
+			unless @subdirs
+				@subdirs = []
+
+				path_info = self[:path_info]
+				break unless path_info
+
+				href = self[:site][:url].to_s
+
+				File.dirname(path_info).split('/').each { |dir|
+					next if dir == '.'
+					href = File.join(href, dir)
+					next if dir.empty?
+
+					@subdirs << {name: dir, href: href}
+				}
+			end
+
+			@subdirs
+		end
+
+		define_key :nav do |key|
+			unless @nav
+				@nav = []
+				@nav << {name: self[:title], href: File.join(self[:site][:url].to_s, '')}
+				@nav += self[:subdirs] if self[:subdirs]
+				@nav << {name: self[:page][:name]} if self[:page][:name]
+				@nav.last[:last?] = true
+			end
+
+			@nav
+		end
+
 	end
 
 end
